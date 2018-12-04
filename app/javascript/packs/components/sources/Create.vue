841d3487e626593eed389c3e8f05905f1278638d
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-12-04 11:36:31 +0900

    add sources/Create.vue #8

---
+++ app/javascript/packs/components/sources/Create.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <source-form></source-form>
+    </div>
+</template>
+
+<script>
+import Form from 'Form.vue';
+export default {
+    components: {
+        'source-form': Form
+    },
+}
+</script>
